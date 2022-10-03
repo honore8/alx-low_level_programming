0155915b89ffa7edf42ef061dad725f06d27c7fe
honore8 <57352746+honore8@users.noreply.github.com>
2022-10-03 21:42:31 +0000

    --

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,38 +1,47 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
 
 /**
- * alloc_grid - allocates a grid, make space and free space
- * @width: takes in width of grid
- * @height: height of grid
- * Return: grid with freed spaces
+ * alloc_grid - Returns a pointer to a 2-dimensional array of
+ *               integers with each element initalized to 0.
+ * @width: The width of the 2-dimensional array.
+ * @height: The height of the 2-dimensional array.
+ *
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the 2-dimensional array of integers.
  */
+int **alloc_grid(int width, int height)
+{
+	int **twoD;
+	int hgt_index, wid_index;
 
-int ** alloc_grid(int width, int height) {
-  /*Declaring variables*/
-  int ** grid;
-  int i, j;
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-  if (width <= 0 || height <= 0) {
-    return (NULL);
-  }
+	twoD = malloc(sizeof(int *) * height);
 
-  grid = malloc(sizeof(int * ) * height); /*malloc*/
+	if (twoD == NULL)
+		return (NULL);
 
-  if (grid == NULL) {
-    return (NULL); 
-  }
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		twoD[hgt_index] = malloc(sizeof(int) * width);
 
-  for (i = 0; i < height; i++) {
-    grid[i] = malloc(sizeof(int) * width);
-    if (grid[i] == NULL) {
-      for (i = i - 1; i >= 0; i--) {
-        free(grid[i]);
-      }
-      free(grid);
-      return (NULL);
-    }
-  }
-  for (i = 0; j < width; j++)
-    grid[i][j] = 0;
-  return (grid);
+		if (twoD[hgt_index] == NULL)
+		{
+			for (; hgt_index >= 0; hgt_index--)
+				free(twoD[hgt_index]);
+
+			free(twoD);
+			return (NULL);
+		}
+	}
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
+	}
+
+	return (twoD);
 }
